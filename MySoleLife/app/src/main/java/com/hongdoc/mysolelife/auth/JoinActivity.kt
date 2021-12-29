a9a1dba64c16f5953eb15867b21771ef0e791b7a
hong_doc <holives7@gmail.com>
2021-12-30 02:13:00 +0900

    로그인 회원가입 작동

---
+++ MySoleLife/app/src/main/java/com/hongdoc/mysolelife/auth/JoinActivity.kt
@@ -0,0 +1,12 @@
+package com.hongdoc.mysolelife.auth
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.hongdoc.mysolelife.R
+
+class JoinActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_join)
+    }
+}
