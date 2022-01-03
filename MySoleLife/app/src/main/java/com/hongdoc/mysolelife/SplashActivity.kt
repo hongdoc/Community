4a5eb01c13816401385bde4436a47e1fdd892a0f
hongdoc <87812112+hongdoc@users.noreply.github.com>
2022-01-03 18:17:57 +0900

    메인화면 꾸미기, Navigation 구현

--- MySoleLife/app/src/main/java/com/hongdoc/mysolelife/SplashActivity.kt
+++ MySoleLife/app/src/main/java/com/hongdoc/mysolelife/SplashActivity.kt
@@ -4,20 +4,47 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Handler
+import android.util.Log
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
 import com.hongdoc.mysolelife.auth.IntroActivity
 
 class SplashActivity : AppCompatActivity() {
+
+    private lateinit var auth: FirebaseAuth
 
     override fun onCreate(savedInstanceState: Bundle?) {
 
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash)
 
+        auth = Firebase.auth
+        // 로그인이 된 상태에서 앱을 켤 시 Intro페이지는 넘어가고 MainActivity로 바로 넘어가게 함
+        if(auth.currentUser?.uid == null){
+
+            Log.d("SplashActivity", "null")
+
+            Handler().postDelayed({
+                startActivity(Intent(this, IntroActivity::class.java))
+                finish()
+            }, 3000)
+
+        } else {
+            Log.d("SplashActivity", "not null")
+
+            Handler().postDelayed({
+                startActivity(Intent(this, MainActivity::class.java))
+                finish()
+            }, 3000)
+
+        }
+
         //처음 화면이 SplashActivity에서 뜨고 3초 이따가 IntroActivity로 넘어가게 한다.
-        Handler().postDelayed({
-            startActivity(Intent(this, IntroActivity::class.java))
-            finish()
-        }, 3000)
+//        Handler().postDelayed({
+//            startActivity(Intent(this, IntroActivity::class.java))
+//            finish()
+//        }, 3000)
 
     }
 
