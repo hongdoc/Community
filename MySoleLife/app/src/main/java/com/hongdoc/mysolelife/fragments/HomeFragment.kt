dcefc5249a82b2894aaac7fb832fdc5b2eccfb5b
hong_doc <holives7@gmail.com>
2022-01-06 02:25:24 +0900

    Navigation 구현 완료

--- MySoleLife/app/src/main/java/com/hongdoc/mysolelife/fragments/HomeFragment.kt
+++ MySoleLife/app/src/main/java/com/hongdoc/mysolelife/fragments/HomeFragment.kt
@@ -1,25 +1,18 @@
 package com.hongdoc.mysolelife.fragments
 
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
 import androidx.databinding.DataBindingUtil
+import androidx.navigation.findNavController
 import com.hongdoc.mysolelife.R
 import com.hongdoc.mysolelife.databinding.FragmentHomeBinding
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
 
-/**
- * A simple [Fragment] subclass.
- * Use the [HomeFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 class HomeFragment : Fragment() {
 
     private lateinit var binding : FragmentHomeBinding
@@ -32,12 +25,24 @@ class HomeFragment : Fragment() {
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
+    ): View {
 
         binding = DataBindingUtil.inflate(inflater, R.layout.fragment_home, container, false)
 
         binding.tipTap.setOnClickListener {
-            Toast.makeText(context, "Clicked", Toast.LENGTH_LONG).show()
+            it.findNavController().navigate(R.id.action_homeFragment_to_tipFragment)
+        }
+
+        binding.talkTap.setOnClickListener {
+            it.findNavController().navigate(R.id.action_homeFragment_to_talkFragment)
+        }
+
+        binding.bookmarkTap.setOnClickListener {
+            it.findNavController().navigate(R.id.action_homeFragment_to_bookmarkFragment)
+        }
+
+        binding.storeTap.setOnClickListener {
+            it.findNavController().navigate(R.id.action_homeFragment_to_storeFragment)
         }
 
         return binding.root
